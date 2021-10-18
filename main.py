e9ea999489ad1ad692c9a6ad1202c8d866b7a1e7
Narongkorn Kitrungrot <psuwit.narongkron.nk@gmail.com>
2021-10-18 23:24:06 +0700

    Add Dictionary to find value

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from tkinter import *
-from tkinter import colorchooser
-from PIL import ImageTk, Image
-import math
 
 root = Tk()
 root.title('Resister calculator')
@@ -9,16 +6,78 @@ root.geometry('500x400+500+200')
 root.resizable(False, False)
 
 dict = {
-    'Black': 0,
-    'Brown': 1,
-    'Red': 2,
-    'Orange': 3,
-    'Yellow': 4,
-    'Green': 5,
-    'Blue': 6,
-    'Violet': 7,
-    'Grey': 8,
-    'White': 9,
+    'Black': {
+        'digit': 0,
+        'multiplier': 1,
+        'tolerance': None,
+        'temperature': 250
+    },
+    'Brown': {
+        'digit': 1,
+        'multiplier': 10,
+        'tolerance': 1,
+        'temperature': 100
+    },
+    'Red': {
+        'digit': 2,
+        'multiplier': 100,
+        'tolerance': 2,
+        'temperature': 50
+    },
+    'Orange': {
+        'digit': 3,
+        'multiplier': 1000,
+        'tolerance': None,
+        'temperature': 15
+    },
+    'Yellow': {
+        'digit': 4,
+        'multiplier': 10000,
+        'tolerance': None,
+        'temperature': 25
+    },
+    'Green': {
+        'digit': 5,
+        'multiplier': 100000,
+        'tolerance': 0.5,
+        'temperature': 20
+    },
+    'Blue': {
+        'digit': 6,
+        'multiplier': 1000000,
+        'tolerance': 0.25,
+        'temperature': 10
+    },
+    'Violet': {
+        'digit': 7,
+        'multiplier': None,
+        'tolerance': 0.1,
+        'temperature': 5
+    },
+    'Grey': {
+        'digit': 8,
+        'multiplier': None,
+        'tolerance': None,
+        'temperature': 1
+    },
+    'White': {
+        'digit': None,
+        'multiplier': None,
+        'tolerance': None,
+        'temperature': None
+    },
+    'Gold': {
+        'digit': None,
+        'multiplier': 0.1,
+        'tolerance': 5,
+        'temperature': None
+    },
+    'Silver': {
+        'digit': None,
+        'multiplier': 0.01,
+        'tolerance': 10,
+        'temperature': None
+    },
 }
 
 FONT = ("Helvetica", 14)
@@ -56,38 +115,52 @@ OptionMenu(root, third_digit, 'Black', 'Brown', 'Red', 'Orange', 'Yellow', 'Gree
 
 multiplier = StringVar()
 multiplier.set('Multiplier')
-OptionMenu(root, multiplier, '1', '10', '100', '1000', '10,000', '100,000', '1,000,000', '0.1', '0.01').place(x=260, y=100)
+OptionMenu(root, multiplier, 'Black', 'Brown', 'Red', 'Orange', 'Yellow', 'Green', 'Blue').place(x=260, y=100)
 
 tolerance = StringVar()
 tolerance.set('Tolerance')
-OptionMenu(root, tolerance, '1%', '2%', '0.5%', '0.25%', '0.1%', '5%', '10%').place(x=360, y=100)
+OptionMenu(root, tolerance, 'Brown', 'Red', 'Green', 'Blue', 'Violet', 'Gold', 'Silver').place(x=360, y=100)
 
+temp1 = 1
+temp2 = 1
+temp3 = 1
+temp4 = 1
+temp5 = 1
 
 def number():
+
+    global temp1, temp2, temp3, temp4, temp5
+
     for x, y in dict.items():
         if first_digit.get() == x:
-            temp1 = y
+            temp1 = y['digit']
         if first_digit.get() != x:
             temp1 = 0
 
         if second_digit.get() == x:
-            temp2 = y
-        if first_digit.get() != x:
+            temp2 = y['digit']
+        if second_digit.get() != x:
             temp2 = 0
 
         if third_digit.get() == x:
-            temp3 = y
+            temp3 = y['digit']
+
+        if multiplier.get() == x:
+            temp4 = y['multiplier']
+
+        if tolerance.get() == x:
+            temp5 = y['tolerance']
 
     num = [temp1, temp2, temp3]
     return int(' '.join(str(num).split(',')).removeprefix('[').removesuffix(']').replace(' ', ''))
 
-
 def ans():
-    return math.floor(number() * int(multiplier.get()) + float(((tolerance.get()).split('%'))[0]) / 100)
+    return str([round(((number() * int(temp4)) + float(temp5 / 100)), 2),
+                round(((number() * int(temp4)) - float(temp5 / 100)), 2)]).replace(',', '-')
 
 
 def made():
-    Label(root, text=f'{number()} * {multiplier.get()} ± {tolerance.get()} = {ans()} Ω', font=FONT).place(x=150, y=70)
+    Label(root, text=f'{number()} * {temp4} ± {temp5} = {ans()} Ω', font=FONT).place(x=100, y=70)
     return None
 
 
