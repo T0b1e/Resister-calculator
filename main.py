31ce19303e63dc8038b7edab16851548c6937401
Narongkorn Kitrungrot <psuwit.narongkron.nk@gmail.com>
2021-10-19 22:37:16 +0700

    Change Image Size and Adding Image in display

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ root.title('Resister calculator')
 root.geometry('500x400+500+200')
 root.resizable(False, False)
 
-dict = {
+dictionary = {
     'Black': {
         'digit': 0,
         'multiplier': 1,
@@ -61,7 +61,7 @@ dict = {
         'temperature': 1
     },
     'White': {
-        'digit': None,
+        'digit': 9,
         'multiplier': None,
         'tolerance': None,
         'temperature': None
@@ -83,10 +83,44 @@ dict = {
 FONT = ("Helvetica", 14)
 PHOTO = 'resistor.png'
 
+
+def about_page():
+    root.destroy()
+    about = Tk()
+    about.title('About Page')
+    about.geometry('500x400+500+200')
+    about.resizable(False, False)
+
+    Label(about, text='Hi my name is Narongkorn Kitrungrot 17', font=10).pack()
+    Label(about, text='and I made this for Educational and Study Tkinter', font=10).pack()
+
+    about.mainloop()
+
+
+def problem_page():
+    root.destroy()
+    problem = Tk()
+    problem.title('About Page')
+    problem.geometry('500x400+500+200')
+    problem.resizable(False, False)
+
+    Label(problem, text='Sending Bug , Error case in text box', font=10).place(x=100, y=100)
+    report = StringVar()
+    Entry(problem, report, text='Sending Report').place(x=100, y=150)
+
+    def reciev():  # TODO
+        Label(problem, text=report.get()).place(x=100, y=200)
+
+    Button(problem, text='Sending Report', command=reciev).place(x=100, y=180)
+
+    problem.mainloop()
+
+
 sub_menu = Menu()
 sub_menu.add_command(label='Resistor color')
 sub_menu.add_command(label='Calculator')
-sub_menu.add_command(label='About')
+sub_menu.add_command(label='Problem', command=problem_page)
+sub_menu.add_command(label='About', command=about_page)
 
 main_menu = Menu()
 root.config(menu=main_menu)
@@ -96,30 +130,36 @@ main_menu.add_cascade(label='Documentation', menu=sub_menu)
 
 Label(root, text='Resister calculator', font=FONT).pack()
 
+img = PhotoImage(file='resister.png')
+Label(
+    root,
+    image=img
+).pack()
+
 first_digit = StringVar()
 first_digit.set('1 Digit')
 OptionMenu(root, first_digit,
            'None', 'Black', 'Brown', 'Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Violet', 'Grey', 'White')\
-    .place(x=20, y=100)
+    .place(x=20, y=200)
 
 second_digit = StringVar()
 second_digit.set('2 Digit')
 OptionMenu(root, second_digit,
            'None', 'Black', 'Brown', 'Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Violet', 'Grey', 'White')\
-    .place(x=100, y=100)
+    .place(x=100, y=200)
 
 third_digit = StringVar()
 third_digit.set('3 Digit')
 OptionMenu(root, third_digit, 'Black', 'Brown', 'Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Violet', 'Grey', 'White')\
-    .place(x=180, y=100)
+    .place(x=180, y=200)
 
 multiplier = StringVar()
 multiplier.set('Multiplier')
-OptionMenu(root, multiplier, 'Black', 'Brown', 'Red', 'Orange', 'Yellow', 'Green', 'Blue').place(x=260, y=100)
+OptionMenu(root, multiplier, 'Black', 'Brown', 'Red', 'Orange', 'Yellow', 'Green', 'Blue').place(x=260, y=200)
 
 tolerance = StringVar()
 tolerance.set('Tolerance')
-OptionMenu(root, tolerance, 'Brown', 'Red', 'Green', 'Blue', 'Violet', 'Gold', 'Silver').place(x=360, y=100)
+OptionMenu(root, tolerance, 'Brown', 'Red', 'Green', 'Blue', 'Violet', 'Gold', 'Silver').place(x=360, y=200)
 
 temp1 = 1
 temp2 = 1
@@ -127,11 +167,12 @@ temp3 = 1
 temp4 = 1
 temp5 = 1
 
+
 def number():
 
     global temp1, temp2, temp3, temp4, temp5
 
-    for x, y in dict.items():
+    for x, y in dictionary.items():
         if first_digit.get() == x:
             temp1 = y['digit']
         if first_digit.get() != x:
@@ -154,16 +195,18 @@ def number():
     num = [temp1, temp2, temp3]
     return int(' '.join(str(num).split(',')).removeprefix('[').removesuffix(']').replace(' ', ''))
 
+
 def ans():
     return str([round(((number() * int(temp4)) + float(temp5 / 100)), 2),
                 round(((number() * int(temp4)) - float(temp5 / 100)), 2)]).replace(',', '-')
 
 
 def made():
-    Label(root, text=f'{number()} * {temp4} ± {temp5} = {ans()} Ω', font=FONT).place(x=100, y=70)
+    Label(root, text=f'{number()} * {temp4} ± {temp5} = {ans()} Ω', font=FONT).place(x=100, y=170)
     return None
 
 
-Button(root, text='Enter', command=made).place(x=220, y=150)
+Button(root, text='Enter', command=made).place(x=220, y=250)
 
-root.mainloop()
+if __name__ == '__main__':
+    root.mainloop()
